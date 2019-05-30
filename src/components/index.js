cda7d7fff18fba068a4bb2d6a475e17ec478b3d5
iamtashi <tacmoktan@gmail.com>
2019-05-30 09:33:47 +0545

    added category container component

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export {default as NavBar} from './NabBar';
+
+export { default as CategoryContainer } from './CategoryContainer';
--- src/pages/global/Home/index.js
+++ src/pages/global/Home/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import workImage from 'assets/images/work.jpg'
+import { makeStyles } from '@material-ui/core/styles';
+//components
+import { CategoryContainer } from "../../../components";
+
+const useStyles = makeStyles({
+    imgContainer:{
+      display:'flex',
+      justifyContent:'center',
+      width:'100%',
+      height:'50%'
+    },
+    image:{
+      width:'inherit',
+      height:'inherit'
+    }
+})
+
 export default function Home() {
+  const { imgContainer, image } = useStyles();
   return (
     <div>
-      <img src={workImage} alt="Work"/>
+      <div className={imgContainer}>
+        <img src={workImage} alt="Work" className={image} />
+      </div>
+      <CategoryContainer />
     </div>
   )
 }
