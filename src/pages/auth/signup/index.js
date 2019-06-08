5e8ad88667a324995209ee39ae16c23904b72422
Subesh Bhandari <subeshb1@gmail.com>
2019-06-08 13:17:34 +0545

    Add Loading and success message

--- src/pages/auth/signup/index.js
+++ src/pages/auth/signup/index.js
@@ -1,189 +1,216 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField, FormControl, InputLabel, OutlinedInput, Select, MenuItem, Button } from '@material-ui/core';
-import { Link, withRouter } from 'react-router-dom';
-import axios from 'axios';
-import { useSnackbar } from 'notistack';
-
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Select,
+  MenuItem,
+  Button
+} from "@material-ui/core";
+import { Link, withRouter } from "react-router-dom";
+import axios from "axios";
+import { useSnackbar } from "notistack";
+import { LoadContext } from "context";
 
 const useStyles = makeStyles({
-    formContainer: {
-        display: 'grid',
-        justifyContent: 'center',
-        maxWidth: "100%",
-    },
-    formHeader: {
-        fontFamily: "'Roboto Condensed', sans-serif ",
-        borderTopLeftRadius: 5,
-        borderTopRightRadius: 5,
-        background: "#37677aa1",
-        borderRight: "12px solid #37677A",
-        borderLeft: "12px solid #37677A",
-        marginTop: 60,      //match this margin with form's marginBottom
-        padding: 12,
-        textAlign: 'center',
-        '& span': {
-            color: 'white'
-        }
-    },
-    form: {
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
-        boxShadow: "0 0 2px grey",
-        display: "grid",
-        justifyContent: "center",
-        minWidth: 310,
-        width: '25vw',
-        padding: "20px 0",
-        marginBottom: 60,
-    },
-    userIcon: {
-        textAlign: 'center',
-        '& i': {
-            height: 60,
-            width: 60,
-            fontSize: 35,
-            border: "2px solid black",
-            borderRadius: "50%",
-            padding: 10
-        }
-    },
-    textField: {
-        display: 'block',
-        '& input': {
-            minWidth: 245
-        }
-    },
-    button: {
-        margin: "5px 0",
-        background: '#212121',
-        color: 'white',
-        '&:hover': {
-            background: 'black'
-        }
+  formContainer: {
+    display: "grid",
+    justifyContent: "center",
+    maxWidth: "100%"
+  },
+  formHeader: {
+    fontFamily: "'Roboto Condensed', sans-serif ",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    background: "#37677aa1",
+    borderRight: "12px solid #37677A",
+    borderLeft: "12px solid #37677A",
+    marginTop: 60, //match this margin with form's marginBottom
+    padding: 12,
+    textAlign: "center",
+    "& span": {
+      color: "white"
     }
-})
+  },
+  form: {
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    boxShadow: "0 0 2px grey",
+    display: "grid",
+    justifyContent: "center",
+    minWidth: 310,
+    width: "25vw",
+    padding: "20px 0",
+    marginBottom: 60
+  },
+  userIcon: {
+    textAlign: "center",
+    "& i": {
+      height: 60,
+      width: 60,
+      fontSize: 35,
+      border: "2px solid black",
+      borderRadius: "50%",
+      padding: 10
+    }
+  },
+  textField: {
+    display: "block",
+    "& input": {
+      minWidth: 245
+    }
+  },
+  button: {
+    margin: "5px 0",
+    background: "#212121",
+    color: "white",
+    "&:hover": {
+      background: "black"
+    }
+  }
+});
 
-function SignUp( {history} ) {
+function SignUp({ history }) {
+  const {
+    formContainer,
+    formHeader,
+    form,
+    userIcon,
+    textField,
+    button
+  } = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const { loading, setLoading } = useContext(LoadContext);
 
-    const { formContainer, formHeader, form, userIcon, textField, button } = useStyles();
-    const { enqueueSnackbar } = useSnackbar();
-    //create state variables
-    const [values, setValues] = React.useState({
-        email: '',
-        password: '',
-        confirm_password: '',
-        type: ''
+  //create state variables
+  const [values, setValues] = React.useState({
+    email: "",
+    password: "",
+    confirm_password: "",
+    type: ""
+  });
+
+  //state variable names should match with event.target.name
+  function handleChange(event) {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
     });
+  }
 
-    //state variable names should match with event.target.name
-    function handleChange(event) {
-        setValues({
-            ...values, [event.target.name]: event.target.value
-        })
-    }
-    
-    const [redirect, setRedirect] = React.useState({
-        status:null
-    });
-    
+  const [redirect, setRedirect] = React.useState({
+    status: null
+  });
 
-    function handleSubmit(event){
-        return axios.post('api/v1/users',values)
-        .then( response => {
-            
-            if ((response.status === 200 )) setRedirect({status:true})
-            
-        })
-        .catch( error => {
-            let message =  error.message.includes(422) ? "Registration Failed" : "Unable to connect to the Server";
-            enqueueSnackbar(message, {variant:"error", autoHideDuration:2500} )
-        } )
-    }
-    
-    React.useEffect(() => {
-        if(redirect.status) history.push('/login');   //redirects to localhost:3000/login
-    })
-    
-    return (
-        <div className={formContainer}>
+  function handleSubmit() {
+    setLoading(true);
+    return axios
+      .post("api/v1/users", values)
+      .then(response => {
+        if (response.status === 200) {
+          setRedirect({ status: true });
+          enqueueSnackbar("Sign Up Successful! Login to continue", {
+            variant: "success",
+            autoHideDuration: 2500
+          });
+        }
+      })
+      .catch(error => {
+        let message = error.message.includes(422)
+          ? "Registration Failed"
+          : "Unable to connect to the Server";
+        enqueueSnackbar(message, { variant: "error", autoHideDuration: 2500 });
+      })
+      .finally(() => setLoading(false));
+  }
 
-            <h1 className={formHeader}>SIGN <span>UP</span></h1>
+  React.useEffect(() => {
+    if (redirect.status) history.push("/login"); //redirects to localhost:3000/login
+  });
 
-            <form className={form} >
-                <div className={userIcon}>
-                    <i className="fas fa-user-tie"></i>
-                </div>
+  return (
+    <div className={formContainer}>
+      <h1 className={formHeader}>
+        SIGN <span>UP</span>
+      </h1>
 
-
-                <TextField
-                    id="email"
-                    className={textField}
-                    label="Email" /* placeholder */
-                    type="email"
-                    name="email"
-                    autoComplete="email"
-                    margin="normal"
-                    variant="outlined"
-                    value={values.email}
-                    onChange={handleChange}
-                />
-
-                <TextField
-                    id="password"
-                    className={textField}
-                    label="Password"
-                    type="password"
-                    name="password"
-                    margin="normal"
-                    variant="outlined"
-                    value={values.password}
-                    onChange={handleChange}
-
-                />
-
-                <TextField
-                    id="confirm-password"
-                    className={textField}
-                    label="Re-enter Password"
-                    type="password"
-                    name="confirm_password"
-                    margin="normal"
-                    variant="outlined"
-                    value={values.confirm_password}
-                    onChange={handleChange}
-
-                />
-
-                <FormControl variant="outlined" style={{ margin: '15px 0' }} >
-                    <InputLabel htmlFor="outlined-account-type">
-                        Account Type
-                    </InputLabel>
-                    <Select
-                        value={values.type}
-                        onChange={handleChange}
-                        input={<OutlinedInput labelWidth={98} name="type" id="outlined-account-type" />}
-                    >
-
-                        <MenuItem value="job_seeker">Job Seeker</MenuItem>
-                        <MenuItem value="job_provider">Job Provider</MenuItem>
-                    </Select>
-                </FormControl>
-
-                <div style={{ textAlign: "right" }}>
-
-                        <Button className={button} onClick={handleSubmit}>
-                            Create an Account
-                        </Button>
-                        
-                </div>
-
-                <Link to="/login" style={{ textAlign: 'right', textDecoration: 'underline' }}>
-                    Already have an Account? Login!
-                </Link>
-            </form>
+      <form className={form}>
+        <div className={userIcon}>
+          <i className="fas fa-user-tie" />
         </div>
-    );
+
+        <TextField
+          id="email"
+          className={textField}
+          label="Email" /* placeholder */
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+          value={values.email}
+          onChange={handleChange}
+        />
+
+        <TextField
+          id="password"
+          className={textField}
+          label="Password"
+          type="password"
+          name="password"
+          margin="normal"
+          variant="outlined"
+          value={values.password}
+          onChange={handleChange}
+        />
+
+        <TextField
+          id="confirm-password"
+          className={textField}
+          label="Re-enter Password"
+          type="password"
+          name="confirm_password"
+          margin="normal"
+          variant="outlined"
+          value={values.confirm_password}
+          onChange={handleChange}
+        />
+
+        <FormControl variant="outlined" style={{ margin: "15px 0" }}>
+          <InputLabel htmlFor="outlined-account-type">Account Type</InputLabel>
+          <Select
+            value={values.type}
+            onChange={handleChange}
+            input={
+              <OutlinedInput
+                labelWidth={98}
+                name="type"
+                id="outlined-account-type"
+              />
+            }
+          >
+            <MenuItem value="job_seeker">Job Seeker</MenuItem>
+            <MenuItem value="job_provider">Job Provider</MenuItem>
+          </Select>
+        </FormControl>
+
+        <div style={{ textAlign: "right" }}>
+          <Button className={button} onClick={() => !loading && handleSubmit()}>
+            Create an Account
+          </Button>
+        </div>
+
+        <Link
+          to="/login"
+          style={{ textAlign: "right", textDecoration: "underline" }}
+        >
+          Already have an Account? Login!
+        </Link>
+      </form>
+    </div>
+  );
 }
 
-export default withRouter(SignUp)
+export default withRouter(SignUp);
