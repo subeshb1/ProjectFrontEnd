80c59a89e4d078fafd26388a0ca1281424016c52
iamtashi <tacmoktan@gmail.com>
2019-06-15 23:21:36 +0545

    Add route for jobseeker home page

--- src/pages/JobSeeker/index.js
+++ src/pages/JobSeeker/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Profile from "./Profile";
 import ProtectedRoute from "components/ProtectedRoute";
-
+import JobPosted from "components/JobPosted";
 
 export default function JobSeeker() {
   return (
     <div>
-      <ProtectedRoute  roles={["job_seeker"]} path="/jobseeker/profile" component={Profile} />
+
+      <ProtectedRoute roles={["job_seeker"]} path="/jobseeker/profile" component={Profile} />
+      <ProtectedRoute roles={["job_seeker"]} path="/jobseeker/home"
+        component={() =>
+
+          <React.Fragment>
+            <JobPosted name="RECOMMENDED" />
+            <JobPosted name="TOP" />
+            <JobPosted name="RECENT" />
+          </React.Fragment>
+
+        } />
+
     </div>
   );
 }
