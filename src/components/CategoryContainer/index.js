690ed7ec3b13a1ae6440eddd7605090a390d4844
iamtashi <tacmoktan@gmail.com>
2019-08-03 15:37:22 +0545

    Improve CategoryContainer with props, NavLink

--- src/components/CategoryContainer/index.js
+++ src/components/CategoryContainer/index.js
@@ -1,47 +1,35 @@
 import React from 'react';
 import { Card, CardContent, CardMedia } from '@material-ui/core';
 import useStyles from '../styles.js';
+import { NavLink } from 'react-router-dom';
 
-const jobs = [
-    { title: "Agriculture", imageName: "agriculture" },
-    { title: "Ayurved", imageName: "ayurved" },
-    { title: "Computer and IT", imageName: "computer and IT" },
-    { title: "Educaiton", imageName: "education" },
-    { title: "Engineering", imageName: "engineering" },
-    { title: "Health", imageName: "health" },
-    { title: "Law", imageName: "law" },
-    { title: "Management", imageName: "management" },
-    { title: "Nursing", imageName: "nursing" },
-    { title: "Pharmacist", imageName: "pharmacist" },
-    { title: "Science", imageName: "science" }
-];
-
-
-function Category() {
+function Category( {title1, title2, jobs} ) {
     let { root, title, titleText, titleLine, card, wrapper, media } = useStyles();
-
+    
     return (
         <div className={root}>
 
             <div className={title}>
                 <span className={titleLine}> </span>
-                JOB  <span className={titleText} >CATEGORIES</span>
+                {title1}   <span className={titleText} > {title2} </span>
                 <span className={titleLine}> </span>
             </div>
 
             <div className={wrapper}>
-                {jobs.map( job =>
+                {jobs.map(job =>
                     //displaying jobs 
                     (
-                        <Card className={card} key={job.title}>
-                            <CardMedia
-                                image={require(`./jobImages/${job.imageName}.jpg`)}
-                                title={`${job.imageName}`} className={media}
-                            />
-                            <CardContent>
-                                {job.title}
-                            </CardContent>
-                        </Card>
+                        <NavLink to={job.link} isActive={() => false}>
+                            <Card className={card} key={job.title}>
+                                <CardMedia
+                                    image={require(`./jobImages/${job.imageName}.jpg`)}
+                                    title={`${job.title}`} className={media}
+                                />
+                                <CardContent>
+                                    {job.title}
+                                </CardContent>
+                            </Card>
+                        </NavLink>
                     )
 
                 )}
