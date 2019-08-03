4224d6d4cc3d5ae3414040816d5a82cbdd03ecef
iamtashi <tacmoktan@gmail.com>
2019-08-03 15:45:02 +0545

    Add ProtectedRoute for SkillTest

--- src/pages/JobSeeker/index.js
+++ src/pages/JobSeeker/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Profile from "./Profile";
 import Stats from "./Stats";
+import SkillTest from "./SkillTest";
 import ProtectedRoute from "components/ProtectedRoute";
 
 
 export default function JobSeeker() {
   return (
     <div>
-      <ProtectedRoute  roles={["job_seeker"]} path="/jobseeker/profile" component={Profile} />
-      <ProtectedRoute  roles={["job_seeker"]} path="/jobseeker/stats" component={Stats} />
+      <ProtectedRoute roles={["job_seeker"]} path="/jobseeker/profile" component={Profile} />
+      <ProtectedRoute roles={["job_seeker"]} path="/jobseeker/stats" component={Stats} />
+      <ProtectedRoute roles={["job_seeker"]} path="/jobseeker/skill" component={SkillTest} />
     </div>
   );
 }
